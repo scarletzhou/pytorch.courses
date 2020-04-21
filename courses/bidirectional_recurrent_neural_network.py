684a3ea6486eaa7f3fa6dcbd53fde6f49879cc69
zhouhan <15053751006@163.com>
2020-04-21 14:24:09 +0800

    bidirectional recurrent neural network

---
+++ courses/bidirectional_recurrent_neural_network.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+
+# 设备配置 cpu or gpu
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# 超参数
+sequence_length = 28
+input_size = 28
+hidden_size = 128
+num_layers = 2
+num_classes = 10
+batch_size = 100
+num_epochs = 2
+learning_rate = 0.003
+
+# MNIST 数据集
+train_dataset = torchvision.datasets.MNIST(root='../../data/',
+                                           train=True,
+                                           transform=transforms.ToTensor(),
+                                           download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='../../data/',
+                                          train=False,
+                                          transform=transforms.ToTensor())
+
+# 数据载入器
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
+
+# 双向循环神经网络 (多对一)
+class BiRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(BiRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(hidden_size * 2, num_classes)  # 2 for bidirection
+
+    def forward(self, x):
+        # 设置初始状态
+        h0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).to(device)  # 2 for bidirection
+        c0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).to(device)
+
+        # 前向传播 LSTM
+        out, _ = self.lstm(x, (h0, c0))  # 输出张量形状： (batch_size, seq_length, hidden_size*2)
+
+        # 解码上一时间的隐藏状态
+        out = self.fc(out[:, -1, :])
+        return out
+
+model = BiRNN(input_size, hidden_size, num_layers, num_classes).to(device)
+
+
+# 损失函数和优化器
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# 训练网络
+total_step = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.reshape(-1, sequence_length, input_size).to(device)
+        labels = labels.to(device)
+
+        # 前向计算
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # 反向计算和优化
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 100 == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                  .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+
+# 测试模型
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.reshape(-1, sequence_length, input_size).to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
+
+# 保存模型
+torch.save(model.state_dict(), 'model.ckpt')
